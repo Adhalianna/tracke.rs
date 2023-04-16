74134698f3064ba0e711811ee86acc67ce9b2633
Adhalianna <natka.goc@gmail.com>
2023-04-16 12:20:34 +0200

    close #43

---
+++ trackers-models/migrations/2023-04-16-095809_test-user/up.sql
@@ -0,0 +1,14 @@
+-- Your SQL goes here
+INSERT INTO users VALUES 
+('00000000-0000-0000-0000-000000000000', 'test@tracke.rs', ' $2a$12$iarbQNjJQwgcuSbtWRI.qejp9Ih42UOq40g.6bM3QfmvaKS3.Aj72'::bytea);
+
+INSERT INTO trackers VALUES
+('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Backlog', true),
+('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000000', 'Household Chores', false);
+
+INSERT INTO tasks VALUES
+('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000001', null, 'Do laundry', 'black + grey colored', 60*60, null, null, ARRAY ['chore']),
+('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000000', now(), 'Do CyberSec assignment', null, 150*60, now(), null, ARRAY ['studies', 'CyberSec', 'assignments']),
+('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000000', null, 'Study for Web Apps test', 'read the flask docs, review REST API guidelines, practice manipulating DOM with JS, review CSS properties used during classes', 240*60, now(), null, ARRAY ['studies', 'Web Apps']),
+('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000001', null, 'Vacuum clean the bedroom', null, 8*60 + 40, null, null, ARRAY ['chore'])
+;
